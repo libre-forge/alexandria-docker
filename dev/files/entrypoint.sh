86aa67a500c2afe66d059bbcd029f91fc0349f35
Mario Garcia <mario.ggar@gmail.com>
2016-12-20 11:01:27 +0100

    Initial commit

---
+++ dev/files/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+tmux -2 new-session -d -s libreforge
+
+# CREATE FRONT WINDOW
+tmux rename-window -t libreforge:0 'front'
+tmux send-keys -t libreforge 'cd' C-m
+
+# CREATE BACK WINDOW
+tmux new-window -t libreforge:1 -n 'back'
+tmux select-window -t libreforge:1
+tmux send-keys -t libreforge 'cd' C-m
+
+# CREATE DATABASE SHELL WINDOW
+tmux new-window -t libreforge:2 -n 'db'
+tmux select-window -t libreforge:2
+tmux send-keys -t libreforge 'cd' C-m
+
+tmux -2 attach-session -t libreforge
